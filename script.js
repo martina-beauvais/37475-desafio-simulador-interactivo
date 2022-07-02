daef74dbf13e397d78dfa26be6b46178e557d4e4
Martina Beauvais <marbeauvais17@gmail.com>
2022-07-02 11:59:02 -0300

    Añadiendo comentarios

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function showMessage() {
+//Mensajes 
+function showMessage() { 
+    alert('¡Hola!')
     alert( 'Vamos a crear una fecha aleatoria' );
 }
   
 showMessage();
+
 
 //CREAR FECHA
 const fecha = new Date(); 
@@ -16,13 +19,11 @@ let getDay, getMonth, getFullYear
 do{
     getDay = parseInt(prompt("Insertar un número del 1 al 31"));
     getMonth = parseInt(prompt("Insertar un número del 1 al 12"));
-    getFullYear = parseInt(prompt("Ingrese un número de cuatro digitos")) 
+    getFullYear = parseInt(prompt("Ingrese un número de cuatro digitos menor a 2022")) 
 
-    if(getMonth <= 12 &&  getDay <= 31 && getFullYear <=2222){
+    if(getMonth <= 12 &&  getDay <= 31 && getFullYear <=2022){ //Se creará una fecha menor o igual a 31/12/2022
         alert("La fecha correspondiente es " +getDay+ "/" +getMonth+ "/" +getFullYear)
-    }else(getMonth <= 0 && getDay <= 0 && getFullYear <= 0 && getFullYear >=2223);{
+    }else{ //De lo contrario, saldrá esta alerta 
         alert("Por favor, ingrese números válidos")
     }
-    
-
-}while((isNaN(getDay) || isNaN(getMonth)) || (getMonth <= 0 || getMonth <= 12 || getDay <= 0 || getDay <= 31 || getFullYear <=2222))
+}while((isNaN(getDay) || isNaN(getMonth)) || (getDay <= 31 || getMonth <=12 ||  getFullYear <=2022))
